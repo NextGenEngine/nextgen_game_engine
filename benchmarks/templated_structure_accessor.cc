8f6e081f1f75cbfc3571030034f8b0dd9cb95c43
Artem Korolev <artkor@live.ru>
2024-04-06 23:54:38 +0300

    testing direct accessors using templates

---
+++ benchmarks/templated_structure_accessor.cc
@@ -0,0 +1,38 @@
+struct Vulkan {
+  int device_id{5};
+};
+
+struct Rendering {
+  Vulkan vulkan;
+};
+
+struct Application {
+  Rendering rendering;
+} app;  // Global instance
+
+template <typename T>
+struct Accessor;
+
+// Specialization for Application
+template <>
+struct Accessor<Application> {
+  // Directly expose Vulkan as a static reference
+  static Vulkan& vulkan;
+};
+
+// You have to define the static member outside the class
+Vulkan& Accessor<Application>::vulkan = app.rendering.vulkan;
+
+#define VULKAN app.rendering.vulkan
+
+auto& VulkanConfig = Accessor<Application>::vulkan;
+
+#include <iostream>
+
+int main() {
+  VulkanConfig.device_id += 3;
+  std::cout << VulkanConfig.device_id << "\n";
+  VulkanConfig.device_id += 8;
+  std::cout << app.rendering.vulkan.device_id << "\n";
+  return 0;
+}
